663f6a5f7866585d22246cc1169c81183e4f0f9d
vinodmathur <49078818+vinodmathur@users.noreply.github.com>
2025-08-23 23:03:42 +0530

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-});
+  plugins: [react()],
+  base: '/',  // ✅ important for Vercel
+})
