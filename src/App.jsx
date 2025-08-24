ce5f6d41928b3c5e9210ae75193baa4a50b582dd
vinodmathur <49078818+vinodmathur@users.noreply.github.com>
2025-08-25 00:59:35 +0530

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Particles from "react-tsparticles";
 import { loadFull } from "tsparticles";
 import { FaFigma, FaSketch, FaHtml5, FaCss3Alt, FaBars } from "react-icons/fa";
 import { SiAdobexd, SiAdobephotoshop, SiCanva } from "react-icons/si";
-import { Typed } from "react-typed";
+import Typed from "react-typed";
 
 export default function App() {
   const [showMore, setShowMore] = useState(false);
