828578e4c0d585c9c293e79aca6fb53fa0b906cc
vinodmathur <49078818+vinodmathur@users.noreply.github.com>
2025-10-15 19:21:06 +0530

    Create useParallax.js

---
+++ src/useParallax.js
@@ -0,0 +1,14 @@
+// useParallax.js
+import { useEffect, useState } from "react";
+
+export const useParallax = () => {
+  const [offsetY, setOffsetY] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => setOffsetY(window.pageYOffset);
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return offsetY;
+};
